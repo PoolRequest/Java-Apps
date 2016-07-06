56259b42a27b1712ff05f6838a56b2a692b1bbfb
Zachary Butts <zacharydbutts@gmail.com>
2016-07-06 00:26:59 -0400

    Update FileCleaner.java

--- FileCleaner/FileCleaner.java
+++ FileCleaner/FileCleaner.java
@@ -1,12 +1,9 @@
-import java.io.File;
-import java.util.Arrays;
-import java.util.Date;
 
 public class FileCleaner {
 
 
 	public static void main(String[] args) {
-		DirectoryReader dLoads = new DirectoryReader();
-
+		new DirectoryReader();
 	}
+
 }
